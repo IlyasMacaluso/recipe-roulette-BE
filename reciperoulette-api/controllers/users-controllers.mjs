832fcc1726d71cd962d24e486f8c4777f5166d2f
IlyasMacaluso <ilyas.macaluso@gmail.com>
2024-07-11 14:23:26 +0200

    Feature update & bug fix
    -Fixed a bug in authorization that would not receive the req.body data correctly
    -Updated the Signup function to also send the token and username, to be able to log the user in immediately after the signup is completed  with success
    -Updated the login function (added array in the second parameter of db.none method for security purpose)

--- reciperoulette-api/controllers/users-controllers.mjs
+++ reciperoulette-api/controllers/users-controllers.mjs
@@ -19,24 +19,35 @@ const getUsers = async (req, res) => {
 const signup = async (req, res) => {
     try {
         const { email, username, password } = req.body
-        const emailExists = await db.oneOrNone(`SELECT email FROM users WHERE email=$1`, email)
-        const usernameExists = await db.oneOrNone(`SELECT username FROM users WHERE username=$1`, username.toLowerCase())
-        
+        const emailExists = await db.oneOrNone(`SELECT email FROM users WHERE email=$1`, [email])
+        const usernameExists = await db.oneOrNone(`SELECT username FROM users WHERE username=$1`, [username.toLowerCase()])
+
         if (!emailExists && !usernameExists) {
             const hashedPassword = await bcrypt.hash(password, 10)
             //creo il nuovo utente utilizzando i dati ricevuti
-            const { id } = await db.one(`INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING id;`, [
+            const user = await db.one(`INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *;`, [
                 username.toLowerCase(),
                 email,
                 hashedPassword,
             ])
-            //assegno una riga di preferences a questo utente e inizializzo le colonne JSON e INTEGER []
+            //assegno una riga di preferences, favorited, e inizializzo le colonne
             await db.none(
                 `INSERT INTO preferences (user_id, blacklisted_ingredients, preferred_cuisines, dietary_preferences, recipes_history) VALUES ($1, $2, $3, $4, $5);`,
-                [id, [], [], { isGlutenFree: false, isVegan: false, isVegetarian: false }, []]
+                [user.id, [], [], { isGlutenFree: false, isVegan: false, isVegetarian: false }, []]
             )
-            await db.none(`INSERT INTO favorited (user_id, favorite_recipes ) VALUES ($1, $2);`, [id, []])
-            res.status(201).json({ id, msg: "User created successfully" })
+            await db.none(`INSERT INTO favorited (user_id, favorite_recipes ) VALUES ($1, $2);`, [user.id, []])
+
+            // assegno un token e lo imposto per l'utente registrato
+            const token = jwt.sign({ id: user.id, username: user.username }, secretKey, { expiresIn: "7d" })
+            await db.none(`UPDATE users SET token=$2 WHERE username=$1`, [username, token])
+
+            res.status(201).json({
+                id: user.id,
+                username: user.username,
+                email: user.email,
+                token,
+                msg: "User created successfully",
+            })
         } else {
             res.status(400).json({ msg: "A user with this email or username already exists" })
         }
@@ -49,17 +60,22 @@ const signup = async (req, res) => {
 const login = async (req, res) => {
     try {
         const { username, password } = req.body
-        const user = await db.oneOrNone(`SELECT * FROM users WHERE username=$1`, [username.toLowerCase()])
+        let user = null
 
-        if (user &&  bcrypt.compare(password, user.password)) {
+        if (username && password) {
+            user = await db.oneOrNone(`SELECT * FROM users WHERE username=$1`, [username.toLowerCase()])
+        }
+
+        if (user && bcrypt.compare(password, user.password)) {
             const token = jwt.sign({ id: user.id, username: user.username }, secretKey, { expiresIn: "7d" })
             await db.none(`UPDATE users SET token=$2 WHERE username=$1`, [username, token])
-            res.status(200).json({ msg: "Logged in", id: user.id, username: user.username, token })
+            res.status(200).json({ msg: "Logged in", id: user.id, username: user.username, email: user.email, token })
         } else {
             res.status(401).json({ msg: "Invalid credentials" })
         }
     } catch (error) {
-        res.status(500).json({ msg: "Internal server error" })
+        console.log(error)
+        res.status(500).json({ msg: error || "Internal server error" })
     }
 }
 
--- reciperoulette-api/utils/authHelpers.mjs
+++ reciperoulette-api/utils/authHelpers.mjs
@@ -2,7 +2,9 @@ import passport from "passport"
 
 const authorize = (req, res, next) => {
     passport.authenticate("jwt", { session: false }, (err, user) => {
+        user = req.body
         if (!user || err) {
+            console.log("error:", err, "user:", user);
             res.status(400).json({ msg: "Unauthorized" })
         } else {
             req.user = user
